7bd1fe4c6ce93ab6e8f1e916386df93e714f03d1
Apara1330 <polapara1330@gmail.com>
2022-03-07 12:04:33 +0530

    [Apara] - To add contacts in list and display

---
+++ Address-Book-System/Address-Book-System/AddressBookDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    public class AddressBookDetails
+    {
+        public char x;
+        public char y;
+        ArrayList allContacts = new ArrayList();
+
+        public void AddDetails()//taking input from address details
+        {
+            Console.WriteLine("Enter First Name: ");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+           
+            Console.WriteLine("Enter State Name: ");
+            string state = Console.ReadLine();
+            
+            Console.WriteLine("Enter City Name: ");
+            string city = Console.ReadLine();
+            
+            Console.WriteLine("Enter Address: ");
+            string address = Console.ReadLine();
+            
+            Console.WriteLine("Enter Email: ");
+            string email = Console.ReadLine();
+            
+            Console.WriteLine("Enter Zip Code: ");
+            int zipCode =int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter Mobile Number: ");
+            long mobileNumber = Convert.ToInt64(Console.ReadLine());
+
+            Address_Book contacts = new Address_Book(firstName, lastName, state, city, address, email, zipCode, mobileNumber);
+            allContacts.Add(contacts);
+        }
+
+        public void view()
+        {
+            foreach (Address_Book contactList in allContacts)
+            {
+                Console.WriteLine(contactList);
+            }
+        }
+    }
+}
--- Address-Book-System/Address-Book-System/Program.cs
+++ Address-Book-System/Address-Book-System/Program.cs
@@ -10,7 +10,29 @@ namespace Address_Book_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM");
+            //Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM");
+            AddressBookDetails addressdetails = new AddressBookDetails();//object to call the methods
+
+            string key = "a";
+
+            while(key != "y")//while loop to repeat the process untill the user done with his work of adding and displaying
+            {
+                Console.WriteLine("Select the options: ");
+                Console.WriteLine("1- Add Contact, 2- Display Contact");
+                int num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1://to add details
+                        addressdetails.AddDetails();
+                        break;
+                    case 2://to view list of all contacts
+                        addressdetails.view();
+                        break;
+                }
+                Console.WriteLine("To Add contacts or to list the contacts, press y/n");
+                key = Console.ReadLine();
+
+            }
             Console.ReadLine();
         }
     }
